43d9c4b76e94ccc11cdf113cfae8830c762515e8
honeypriya6741 <honeypriya6741@gmail.com>
2023-04-17 11:09:30 +0530

    fixed mail

--- js/main.js
+++ js/main.js
@@ -2015,3 +2015,36 @@
 
   
 })(jQuery);
+
+// js validation
+
+
+
+let sub_btn = document.querySelector('.myform');
+sub_btn.addEventListener('submit', function(event){
+    event.preventDefault();
+    debugger;
+    let fname = document.querySelector('#name').value;
+    let email = document.querySelector('#email').value;
+    let selectElement = document.getElementById('opselection').value;
+    let phone = document.querySelector('#phone').value;
+    let message = document.querySelector('#message').value;
+
+    let subject ="get a quote"
+    Email.send({
+       Host : "smtp.elasticemail.com",
+       Username : "raagainfra.website@gmail.com",
+       Password : "53D36010792280C9D90A5D421B21F726B4D0",
+       To : 'priya4241honey@gmail.com',
+       From : "raagainfra.website@gmail.com",
+       Subject :"get a quote",
+       Body :"Name:"+ fname +"<br/> For email:" + email +"<br/> Subject:" + subject +"<br/> Message:"
+       + message +"<br/> phone:"+ phone + "<br/> service:"+ selectElement
+   }).then(
+     message => alert(message)
+   );
+   
+
+})
+
+
